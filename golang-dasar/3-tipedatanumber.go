deb0cbab96e1bec3b3f259cdb4b9e3e79b830eb7
Veronica Maya Santi <veronicamayasanti@gmail.com>
2025-02-14 22:25:21 +0700

    tipe data number

---
+++ golang-dasar/3-tipedatanumber.go
@@ -0,0 +1,19 @@
+// tipe data number :
+// integer : int8, int16, int32, int64. uint8, uint16, uint32, uint64
+// floating point: float32, float64, complex64, complex128
+// Alias
+// tipe data			alias untuk
+// byte					uint8
+// rune					int32
+// int					minimal int32
+// uint					minimal uint32
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma lima = ", 3.5)
+}
