15aa869bb476f237feb50fbc01ddd840b7137381
Veronica Maya Santi <veronicamayasanti@gmail.com>
2025-03-10 05:19:07 +0700

    struct

---
+++ golang-dasar/32 komentar.go
@@ -0,0 +1,13 @@
+package main
+
+//	komentar terbaik pada kode adalah kode itu sendiri
+//	 saat membuat kode, perlu membuat kode semudah mungkin untuk dibaca
+//	namun kadang juga butuh menambahkan komentar di kode
+
+/**
+this is multi line comment
+you can add a lot of comments here
+here
+*/
+
+// this is single ;ine comment
---
+++ golang-dasar/33 struct.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+//	struct adalah sebuah template data yang digunakan untuk menggabungkan no atau lebih tipe data lainnya dalam satu kesatuan
+//	struct biasanya representasi data dalam program aplikasi yang kita buat
+//	data di struct disimpan dalam field
+//	struct adalah kumpulan dari field
+
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+//	struct adalah template data atau prototype data
+//	struct tidak bisa langsung digunakan
+//	namun kita bisa membuat data/object dari struct yang telah dibuat
+
+func main() {
+	var maya Customer
+	maya.Name = "maya dev"
+	maya.Address = "tendjo"
+	maya.Age = 33
+
+	fmt.Println(maya)
+	fmt.Println(maya.Name)
+	fmt.Println(maya.Address)
+	fmt.Println(maya.Age)
+
+	santi := Customer{
+		Name:    "santi",
+		Address: "bogor",
+		Age:     22,
+	}
+	fmt.Println(santi)
+
+	veronica := Customer{"veronica", "jakarta", 34}
+	fmt.Println(veronica)
+
+}
