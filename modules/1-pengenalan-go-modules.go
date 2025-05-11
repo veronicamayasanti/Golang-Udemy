6bda42624f077d5554209a8c5a14d1fa2e2032c1
Veronica Maya Santi <veronicamayasanti@gmail.com>
2025-05-11 14:46:48 +0700

    sebelum ada go-modules

--- modules/1-pengenalan-go-modules.go
+++ modules/1-pengenalan-go-modules.go
@@ -1,3 +1,10 @@
 package modules
 
 //	SEBELUM ADA GO MODULES
+//	saat membuat aplikasi menggunakan library dari project lain
+//	meng-copy semua source code library atau dependency lain ke project kita,
+//	hal ini memebuat project kita menjadi bengkak karena penuh dengan library orang lain
+//	atau menyimpan di GOPATH directory, namun hal ini aka sulit jika ternyata beberapa aplikasi butuh library yang sama dengan versi yang berbeda
+
+//	 Go-lang modules mulai dikenalkan di go-lang 1.11 dan 1.12
+//	dengan Go-Modules bisa membuat project dengan mudah dan dependency management yang sangat mudah
