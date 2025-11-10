5fb710df9cc3c16a69058483c41f04b74a4eb19c
Veronica Maya Santi <veronicamayasanti@gmail.com>
2025-11-10 15:59:14 +0700

    mengagalkan test

--- unit-test/helper/hello-world_test.go
+++ unit-test/helper/hello-world_test.go
@@ -1,10 +1,28 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldVeronica(t *testing.T) {
+	result := HelloWorld("maya")
+
+	if result != "Hello maya" {
+		//panic("result is not 'Hello maya'")
+		//t.Fail()
+		t.Error("result must be 'Hello maya'")
+	}
+	fmt.Println("TestHelloWorldVeronica done", result)
+}
+
+func TestHelloWorldMaya(t *testing.T) {
 	result := HelloWorld("maya")
 	if result != "Hello maya" {
-		panic("result is not 'Hello maya'")
+		//error
+		//panic("result is not 'Hello maya'")
+		//t.FailNow()
+		t.Fatal("result must be 'Hello maya'")
 	}
+	fmt.Println("TestHelloWorldMaya done")
 }
